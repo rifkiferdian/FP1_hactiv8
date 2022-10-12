18393c8440408c3aeb4e2d4838dd25581d9079b2
Ahmad Rifki <rifkiferdian.111@gmail.com>
2022-10-12 14:04:39 +0700

    buat server

--- app.js
+++ app.js
@@ -7,6 +7,13 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get("/", (req, res) => {
+    const data = {
+            kelompok : 3,
+    }
+    res.status(200).json({'Final Project 1 Hactiv8':data});
+});
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
