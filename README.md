4f11464c6b2af15de584331f420214fb899c45d4
farhan <farhanafriza24@gmail.com>
2022-10-13 14:08:31 +0700

    add database connection

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ Final Project 1 Havtiv8
 Membuat API dengan database POstgreSQL
 
 🌱 Kelompok 3
+
+## Notes
+
+Untuk terhubung ke database `rename file .env.example` menjadi `.env`
+
+
+### Task
+- [x] Membuat database dan tabel.
+- [x] Konfigurasi environment untuk terhubung ke database postgres.
+- [ ] Membuat Autentikasi dan Autorisasi menggunakan JWT (Middleware)
+- [ ] Create API login
+- [ ] Create API register
+- [ ] Create API CRUD reflections
+
+### Read Docs
+
+[Node Postgres](https://node-postgres.com/features/connecting);
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
+require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 4000;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
@@ -14,6 +14,6 @@ app.get("/", (req, res) => {
     res.status(200).json({'Final Project 1 Hactiv8':data});
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`App listening at http://localhost:${process.env.PORT}`);
 });
